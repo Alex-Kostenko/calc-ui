2cf1adc8c74f3ec84ee2ce96b634352ef86d8945
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-18 14:51:34 +0200

    add dynamic header props

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,44 @@
+import Breadcrumbs from "@/components/Breadcrumbs";
 import Footer from "@/components/Footer";
 import Header from "@components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+
+interface HeaderOptions {
+  buttonName: string;
+  href: string;
+  onClick: (...props: any) => any;
+}
 
 const Layout = () => {
+  const location = useLocation();
+  const path = location.pathname;
+
+  const headerOptions: Record<string, HeaderOptions> = {
+    "/": {
+      buttonName: "Log Out",
+      href: "/login",
+      onClick: () => {
+        localStorage.removeItem("token");
+      },
+    },
+    "/login": {
+      buttonName: "register",
+      href: "/register",
+      onClick: () => {},
+    },
+    "/register": {
+      buttonName: "login",
+      href: "/login",
+      onClick: () => {},
+    },
+  };
+
   return (
     <>
-      <Header />
-      <main className="min-h-[calc(100vh-120px)]">
-        <div className="w-12 bg-white"></div>
+      <Header {...headerOptions[path]} />
+      <main className="min-h-[calc(100vh-120px)] flex flex-col">
+        <div className="w-full h-16 bg-white"></div>
+        <Breadcrumbs />
         <Outlet />
       </main>
       <Footer />
