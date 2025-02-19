f00b77f6857aad483f00097e7d50ea6a628cf74c
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-19 12:26:18 +0200

    add result styles

--- src/components/Calculator/Result.tsx
+++ src/components/Calculator/Result.tsx
@@ -10,27 +10,34 @@ const Result = () => {
   console.log(location);
 
   return (
-    <Container className="grid grid-cols-2 gap-5">
-      <p>car price: {carPrice}</p>
-      <p>vat: by formula</p>
-      <p>auction tax: not available</p>
-      <p>broker: not available</p>
-      <p>insurance: not available</p>
-      <p>expedition: not available</p>
-      <p>port delivery price: {location?.price}</p>
-      <p>city delivery price: not available</p>
-      <p>
-        sea transportation price:{" "}
-        {carType?.name &&
-          location?.port &&
-          location?.port.car_types.find((type) => type.name === carType.name)!
-            .price}
-      </p>
-      <p>certification: by final sum</p>
-      <p>excise: by formula</p>
-      <p>registration: by final sum</p>
-      <p>duty: by formula</p>
-      <p>company service: not available</p>
+    <Container className="grid grid-cols-2 gap-5 bg-main-gray text-secondary-gray pt-4 rounded">
+      <div className="flex flex-col gap-4 px-2">
+        <p>car price: {carPrice}</p>
+        <p>auction tax: not available</p>
+        <p>insurance: not available</p>
+        <p>port delivery price: {location?.price}</p>
+        <p>
+          sea transportation price:{" "}
+          {carType?.name &&
+            location?.port &&
+            location?.port.car_types.find((type) => type.name === carType.name)!
+              .price}
+        </p>
+        <p>excise: by formula</p>
+        <p>duty: by formula</p>
+      </div>
+
+      <div className="flex flex-col gap-4 px-2">
+        <p>vat: by formula</p>
+        <p>broker: not available</p>
+        <p>expedition: not available</p>
+        <p>city delivery price: not available</p>
+        <p>certification: by final sum</p>
+        <p>registration: by final sum</p>
+        <p>company service: not available</p>
+      </div>
+
+      <div className="col-span-2 px-24 py-1 text-white bg-blue-600">total</div>
     </Container>
   );
 };
