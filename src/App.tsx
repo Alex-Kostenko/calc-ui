6ce75a2911fde132f8c0479d0333bd6aaba0a589
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-20 10:36:37 +0200

    add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "@layouts/Layout";
 import Login from "@pages/Login";
-import Register from "./pages/Register";
-import Calculator from "./pages/Calculator";
+import Register from "@pages/Register";
+import Calculator from "@pages/Calculator";
+import ProtectedRoute from "@components/PortectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Calculator />} />
+        <Route element={<ProtectedRoute />}>
+          <Route index element={<Calculator />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Route>
