3229a1af89ee14558637b49af6504903902ffaed
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-23 09:28:00 +0200

    set values on select change

--- src/components/Calculator/EngineVolume.tsx
+++ src/components/Calculator/EngineVolume.tsx
@@ -1,11 +1,22 @@
 import { Input } from "antd";
 import Container from "../Container";
+import { useAppDispatch } from "@/hooks";
+import { setAll } from "@/store/slices/total.slice";
 
 const EngineVolume = () => {
+  const dispatch = useAppDispatch();
+
+  const handleSelect = (value: number) => {
+    dispatch(setAll({ engineVolume: value }));
+  };
   return (
     <Container>
       <h3>Engine Volume:</h3>
-      <Input type="number" />
+      <Input
+        type="number"
+        onChange={(e) => handleSelect(+e.target.value)}
+        min={10}
+      />
     </Container>
   );
 };
--- src/components/Calculator/Year.tsx
+++ src/components/Calculator/Year.tsx
@@ -1,17 +1,27 @@
 import { Select } from "antd";
 import Container from "../Container";
+import { useAppDispatch } from "@/hooks";
+import { setAll } from "@/store/slices/total.slice";
 
 const Year = () => {
-  const selectLength = new Date().getFullYear() - 1990;
+  const startYear = 1990;
+  const selectLength = new Date().getFullYear() - startYear;
+
+  const dispatch = useAppDispatch();
+
+  const handleSelect = (value: number) => {
+    dispatch(setAll({ carYear: value }));
+  };
 
   return (
     <Container>
       <h3>Year:</h3>
       <Select
+        onSelect={handleSelect}
         options={Array.from({ length: selectLength })
           .map((_, i) => ({
-            title: String(1990 + i),
-            value: 1990 + i,
+            title: String(startYear + i),
+            value: startYear + i,
           }))
           .reverse()}
       />
