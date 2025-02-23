d6ececbd22b242515702a5f2974daa0d66e424dc
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-23 09:32:36 +0200

    set user data

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -11,8 +11,15 @@ import {
   CarType,
   CarContainer,
 } from "@/components/Calculator/index";
+import { useAppDispatch } from "@/hooks";
+import { useGetMeQuery } from "@/store/api";
+import { setAll } from "@/store/slices/total.slice";
 
 const Calculator = () => {
+  const dispatch = useAppDispatch();
+  const { data } = useGetMeQuery();
+  dispatch(setAll({ user: data }));
+
   return (
     <div className="grid lg:grid-cols-2 gap-5 container mx-auto my-10">
       <div className="flex flex-col gap-5">
