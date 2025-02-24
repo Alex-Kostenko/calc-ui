1645926b7112f9611b9888d41b33e13af9cca168
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-02-24 18:13:16 +0200

    fix port display

--- src/components/Calculator/Port.tsx
+++ src/components/Calculator/Port.tsx
@@ -5,10 +5,7 @@ const Port = () => {
   const location = useAppSelector((state) => state.total.location);
   return (
     <Container>
-      <h3>Port:</h3>
-      <div className="bg-white min-h-[30px] border border-[#d9d9d9] m-0 px-[11px] py-[4px] text-[rgba(0,0,0,0.88)] text-[14px] leading-[1.5] list-none relative inline-block w-full min-w-0 rounded-md transition-all duration-200">
-        {location?.port.name}
-      </div>
+      <h3>Port: {location?.port.name}</h3>
     </Container>
   );
 };
