da6a116297843f22342820afa17e81b90b69a8f5
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-04 12:28:38 +0200

    reset location on auction change

--- src/components/Calculator/Locations.tsx
+++ src/components/Calculator/Locations.tsx
@@ -3,18 +3,33 @@ import { useGetAllLocationsQuery } from "@/store/api";
 import { setAll } from "@/store/slices/total.slice";
 import { Select } from "antd";
 import Container from "../Container";
+import { useEffect, useState } from "react";
 
 const Locations = () => {
+  const [value, setValue] = useState<string | null>(null);
+
   const { data, isLoading } = useGetAllLocationsQuery();
+
   const auctionName = useAppSelector((state) => state.total.auctionName);
+
   const dispatch = useAppDispatch();
+
   const handleSetPorts = (locationName: string) => {
     dispatch(
       setAll({
         location: data?.data.find((location) => location.name === locationName),
       })
     );
+
+    setValue(locationName);
   };
+
+  const clear = () => setValue(null);
+
+  useEffect(() => {
+    clear();
+  }, [auctionName]);
+
   return (
     <Container>
       <h3>Локація:</h3>
@@ -22,6 +37,8 @@ const Locations = () => {
         <Select
           onSelect={handleSetPorts}
           className="w-full"
+          allowClear
+          value={value}
           options={
             data?.data
               .filter((l) => l.auctions.some((a) => a.name === auctionName))
