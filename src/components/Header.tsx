7008419e0546625c7ebc35d8433b3d09509d6dcc
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-11 10:03:40 +0200

    fix header button position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({ buttonName, href, onClick }: Props) => {
         <Button
           onClick={onClick}
           size="large"
-          className="absolute z-50 bg-white top-12 -translate-x-full left-full flex items-center w-fit px-4 md:px-6 lg:px-8 shadow-md"
+          className="absolute z-50 bg-white top-8 -translate-x-[116%] left-full flex items-center w-fit px-4 md:px-6 lg:px-8 shadow-md"
         >
           <a href={href}>{buttonName}</a>
         </Button>
