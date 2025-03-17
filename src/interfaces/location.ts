ff271cf967d9eb6b7d9db2e8b02664e58bb1cf7a
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-17 12:04:48 +0200

    fix interface

--- src/interfaces/location.ts
+++ src/interfaces/location.ts
@@ -2,11 +2,12 @@ import { IAuction } from "./auction";
 import { IEntity } from "./entity";
 import { IPort } from "./port";
 import { IState } from "./state";
+import { ICarPrice } from "./car.type";
 
 export interface ILocation extends IEntity {
   name: string;
-  price: number;
   port: IPort;
   state: IState;
   auctions: IAuction[];
+  price_by_type: ICarPrice[];
 }
