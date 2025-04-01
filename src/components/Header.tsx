64cd453818e9c175a3451351bc85f891e6edbf37
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-04-01 11:53:15 +0300

    add user greeting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useGetMeQuery } from "@/store/api";
 import { Button } from "antd";
 
 interface Props {
@@ -7,17 +8,20 @@ interface Props {
 }
 
 const Header = ({ buttonName, href, onClick }: Props) => {
+  const { data: user } = useGetMeQuery();
+
   if (buttonName === "none") return <></>;
   return (
     <header>
       <nav className="h-0 container mx-auto">
-        <Button
-          onClick={onClick}
-          size="large"
-          className="absolute z-50 bg-white top-8 -translate-x-[116%] left-full flex items-center w-fit px-4 md:px-6 lg:px-8 shadow-md"
-        >
-          <a href={href}>{buttonName}</a>
-        </Button>
+        <div className="gap-5 absolute z-50  top-8 -translate-x-[116%] left-full flex items-center w-fit px-4 md:px-6 lg:px-8">
+          <div className="bg-white px-4 py-1.5 rounded-md shadow-md text-nowrap border border-transparent">
+            Привіт, {user && user.username}
+          </div>
+          <Button onClick={onClick} size="large" className="shadow-md">
+            <a href={href}>{buttonName}</a>
+          </Button>
+        </div>
       </nav>
     </header>
   );
